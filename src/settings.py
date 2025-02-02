e2ebd8f40482f4df7e568bd67dfa14fc5f86b345
VanekPetr <petrr.vanekk@gmail.com>
2025-02-02 18:26:47 +0100

    chore: switch model

--- src/settings.py
+++ src/settings.py
@@ -7,7 +7,7 @@ load_dotenv()
 class Settings(BaseSettings):
     NAME: str = "Pension Planning Agent"
     OPEN_ROUTER_API_KEY: str = "open-key"
-    LLM_MODEL: str = "google/gemini-2.0-flash-exp:free"
+    LLM_MODEL: str = "google/gemini-flash-1.5-8b"
 
 
 settings = Settings()
